33ef086f5b85170741dbc2a6ef06129b03b5ab9a
garikapati-vijitha <garikapativijithachowdary2004@gmail.com>
2026-07-04 13:53:35 +0530

    Add files via upload

---
+++ Run Time input.py
@@ -0,0 +1,180 @@
+# RUN TIME INPUT()
+# PRINT_TYPE
+
+"print(2+3)"
+
+"""a=4
+b=9
+print(a+b)"""
+
+
+"""a=4
+b=9
+c=a+b
+print(c)"""
+
+# RUN TIME INPUT()
+
+'''a=int(input("enter the a value : "))
+b=int(input("enter the b value : "))
+print(a+b)'''
+
+'''a=float(input("enter the a value : "))
+b=float(input("enter the b value : "))
+print(a+b)'''
+
+'''a=str(input("data : "))
+b=str(input("data1 "))
+print(a+b)'''
+
+# the below cmd is used for the string
+'''a=(input("data : "))
+b=(input("data1 "))
+print(a+b)'''
+
+'''a=(input("data : "))
+b=(input("data1 "))
+print(a+b)'''
+
+'''fname=(input("first_name : "))
+lname=(input("last_name : "))
+print(fname+lname)'''
+
+'''a=bool(input("data : "))
+b=bool(input("data1 "))
+print(a+b)'''
+
+'''a=complex(input("data : "))
+b=complex(input("data1 "))
+print(a+b)'''
+
+'''a=bool(input("data"))
+b=bool(input("data1"))
+print(a+b)'''
+
+'''a=bool(input("data"))
+b=bool(input("data1"))
+c=bool(input("data2"))
+print(a+b+c)'''
+
+# USER_INPUT()
+
+'''a=int(input("a value : "))
+b=int(input("b value : "))
+option=int(input("choose the option 1.add 2.sub 3.mul 4.div"))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)'''
+
+'''a=int(input("a value : "))
+b=int(input("b value : "))
+option=int(input("""choose the option 1.add
+                              2.sub
+                              3.mul
+                              4.div"""))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)'''
+
+'''a=int(input("a value : "))
+b=int(input("b value : "))
+option=input("choose the option add sub mul div")
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)'''
+
+'''a=int(input())
+b=int(input())
+print(a+b)'''
+
+
+'''a=input()
+print(a)'''
+
+
+#SWIPPING_OF_TWO_VARIABLES()
+
+# swapping integer
+# using temp variable
+'''a=10
+b=20
+temp=a
+a=b
+b=temp
+print("a value is :",a)
+print("b value is :",b)'''
+#using arethimatic operators
+'''a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print("a value is :",a)
+print("b value is :",b)'''
+#using bitwise operators
+'''a=10
+b=20
+a=a^b
+b=b^a
+a=a^b
+print("a value is :",a)
+print("b value is :",b)'''
+
+#using numericnumber formatting
+'''a=10
+b=20
+a=a^b
+b=b^a
+a=a^b
+print("after swapping a=%d b=%d" %(a,b))'''
+
+
+#string swapping
+# using temp variable
+'''a="vijitha"
+b="surjana"
+temp=a
+a=b
+b=temp
+print("a value is :",a)
+print("b value is :",b)'''
+
+'''a="vijitha"
+b="surjana"
+a,b=b,a
+print("after swapping a=%s b=%s" %(a,b))'''
+
+# float swapping\a=10.245
+b=20.236
+temp=a
+a=b
+b=temp
+print("a value is :",a)
+print("b value is :",b)'''
+
+
+a=10.245
+b=20.236
+a=a+b
+b=a-b
+a=a-b
+print("a value is :",a)
+print("b value is :",b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

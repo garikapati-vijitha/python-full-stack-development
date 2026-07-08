f8cbf44459f7641fcceffa5cdb170ad419e19cb7
garikapati-vijitha <garikapativijithachowdary2004@gmail.com>
2026-07-08 13:53:59 +0530

    Add files via upload

---
+++ if else real life conditions.py
@@ -0,0 +1,137 @@
+# if else:
+# voting 
+'''age = int(input("The age of the person: "))
+if age >=18:
+    print("eligible for vote")
+else:
+    print("not eligible for vote")'''
+
+# even or odd
+'''num = int(input("number: "))
+if num%2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")'''
+
+# leap year
+'''year = int(input("The year: "))
+if year%4 ==0:
+    print("Leap year")
+else:
+    print("not a leap year")'''
+
+#Guest code
+'''name = input("name: ")
+if name == "vijitha":
+    print("welcome vijitha")
+else:
+    print("welcome guest")'''
+
+'''name = input("name: ").lower
+if name == "vijitha":
+    print("welcome",name)
+else:
+    print("welcome guest")'''
+
+# list
+# with 5 names
+'''names = ["vijitha","surjana","sunitha","yashawini","shobha"]
+a = input("name: ")
+if a in names:
+    print("welcome",a)
+else:
+    print("welcome guest")'''
+
+# vowels
+'''vowels = ["a","e","i","o","u"]
+v = input("letter: ")
+if v in vowels:
+    print("vowel")
+else:
+    print("consonant")'''
+
+# nested if
+# social media login
+'''user_name = input("enter the user name")
+password = input("enter the password")
+if user_name == password:
+    print("login Successfully")
+    if user_name != password:
+         print("invalid credentials")
+else:
+    print("invalid credentials")'''
+
+"""user_name = input("enter the user name")
+password = input("enter the user name")
+if user_name == "vijitha":
+    if password == 12345:
+        print("login successfull")
+    else:
+        print("invalid password")
+else:
+    print("invalid user_name")"""
+
+'''a = "vijitha"
+b = "srujana"
+username = input("enter username")
+password = input("enter password")
+if username ==a:
+    if password == b:
+        print("login successfull")
+    else:
+        print("incorrect password")
+else:
+    print("invaild username")'''
+
+'''username = input('enter the username')
+password = int(input('enter the password'))
+if username=="vijitha" and password == "1234":
+    print("login successfull")
+else:
+    print("invaild credentials")'''
+
+# multiple if
+'''age = int(input("enter the age: "))
+marks = int(input("enter the marks: "))
+attendence = int(input("enter the attendence: "))
+if age >=18:
+    print("eligible for vote")
+if marks >= 80:
+    print("eligible for scholarship")       
+if attendence >=70:
+    print("allow for exams")'''
+
+'''age = int(input("enter the age: "))
+marks = int(input("enter the marks: "))
+attendence = int(input("enter the attendence: "))
+if age >=18:
+    print("eligible for vote")
+else:
+    print("not eligible for vote")
+if marks >= 80:
+    print("eligible for scholarship")
+else:
+    print("not eligible for scholarship")        
+if attendence >=70:
+    print("allow for exams")
+else:
+    print("not eligible for exams")'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

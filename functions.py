8ed07ef37b38bf3335506bf0a82f986ae859384e
garikapati-vijitha <garikapativijithachowdary2004@gmail.com>
2026-07-17 20:36:36 +0530

    Add files via upload

--- functions.py
+++ functions.py
@@ -148,53 +148,274 @@ splitbill()'''
 splitbill()'''
 
 # add, sub,mul
-'''def calculation():
+'''while True:
+    def calculation():
+        a=int(input("a value: "))
+        b=int(input("b value: "))
+        option=int(input("option 1.add 2.sub 3.mul"))
+        if option == 1:
+            print(a+b)
+        elif option == 2:
+            print(a-b)
+        elif option == 3:
+            print(a*b)
+        else:
+            print("Invaild option")
+    calculation()'''
+
+# multiple keyword
+'''def add():
+    print(a+b)
+def sub():
+    print(a-b)
+def mul():
+    print(a*b)
+while True:
     a=int(input("a value: "))
     b=int(input("b value: "))
     option=int(input("option 1.add 2.sub 3.mul"))
-    if option == 1:
-        print(a+b)
+    if option == 1 :
+        add()
     elif option == 2:
-        print(a-b)
+        sub()
     elif option == 3:
-        print(a*b)
-    else:
-        print("Invaild option")
-calculation()'''
+        mul()'''
+
+# keyword and position arguments
+ 
+'''def Details(id,name,mailid):
+    id=1
+    name="vijitha"
+    mailid="v@gmail.com"
+    print(id,name,mailid)
+Details(id="id",name="name",mailid="mailid")'''# step1
 
 
+'''def Details(id,name,mailid):
+    print(id,name,mailid)
+Details(id="id",name="name",mailid="mailid")
+Details(id=1,name="vijitha",mailid="v@gmail.com")
+Details(id=2,name="srujana",mailid="s@mail.com") # step2
+Details(3,"yashu","y@gmail.com")#step3
+Details("sunitha","su@gmail.com",4)#step4
+Details(mailid="v@gmail.com",id=4,name="vijitha")'''#step5
 
+# default arguments
+'''def Grocery(item,price):
+    print("item is %s" %item)
+    print("price is %.2f" %price)
+Grocery("sugar",100)'''
 
+'''def Grocery(item="rice",price=1500):
+    print("item is %s" %item)
+    print("price is %.2f" %price)
+Grocery()'''
 
+'''def Grocery(item,price=200):
+    print("item is %s" %item)
+    print("price is %.2f" %price)
+Grocery("dhal")'''
 
+'''def Grocery(item="ghee",price):
+    # non def arg follows def arg
+    print("item is %s" %item)
+    print("price is %.2f" %price)
+Grocery(2000)'''
 
+# real life project
+# cake_name,price,qty
+'''def Bakery(cake_name="vennila",price=200,qty=1):
+    print("cake_name is %s" %cake_name)
+    print("price is %.2f" %price)
+    print("qty is %d kg/g" %qty)
+Bakery()'''
 
+'''def Bakery(cake_name,price,qty):
+    print("cake_name is %s" %cake_name)
+    print("price is %.2f" %price)
+    print("qty is %d kg/g" %qty)
+Bakery(cake_name="butter scotch",price=200,qty=1.5)'''
 
+'''def Bakery(cake_name,price,qty):
+    print("cake_name is %s" %cake_name)
+    print("price is %.2f" %price)
+    print("qty is %d kg/g" %qty)
+Bakery(200,2)'''# error
 
+'''def Bakery(cake_name,price=500,qty=3):
+    print("cake_name is %s" %cake_name)
+    print("price is %.2f" %price)
+    print("qty is %d kg/g" %qty)
+Bakery(butter scotch)'''
 
+# *(star) arguments (* is used to unpack the elements)
+'''a=[2,3,4,5,6,8,9]
+print(a)
+print(*a)'''
 
+'''a=(2,3,4,5,6,8,9)
+print(a)
+print(*a)'''
 
+'''a={2,3,4,5,6,8,9}
+print(a)
+print(*a)'''
 
+'''b={"name":"vijitha","city":"vij"}
+print(b)
+print(*b)'''
 
+'''c="python"
+print(c)
+print(*c)'''
 
+'''a,b,c=2,3,4,5,6,7,8,9,10
+print(a)
+print(b)
+print(c)'''# error
 
+'''a,b,c=2,3,4
+print(a)
+print(b)
+print(c)'''
 
+'''*a,b,c=2,3,4,5,6,7,8,9,10
+print(*a)
+print(b)
+print(c)'''
 
+'''a,*b,c=2,3,4,5,6,7,8,9,10
+print(a)
+print(*b)
+print(c)'''
 
+'''a,b,*c=2,3,4,5,6,7,8,9,10
+print(a)
+print(b)
+print(*c)'''
 
+'''*a,b,*c=2,3,4,5,6,7,8,9,10
+print(*a)
+print(b)
+print(*c)''' # error
 
+'''a,b,c="vijitha"
+print(a)
+print(b)
+print(c)''' #error
 
+'''a,b,c="vij"
+print(a)
+print(b)
+print(c)'''
 
+'''a,b,*c="vijitha"
+print(a)
+print(b)
+print(*c)'''
 
+# VARIABLE LENGTH ARGUMENT
+# VARIBLE LENGTH ARUGUMENT AUTOMATICALY STORE IN TUPLE AND WE USE * ARGUMENTS
 
+'''def check(*a):
+    print(a)
+    print(type(a))
+check()
+check(2,3,4,5,6,7,8,9)
+b=[4,5,6,7,8]
+check(*b)
+c={1,2,3,4,5,6,7,8}
+check(*c)
+d={"name":"vijitha","age":21,"place":"vja"}
+check(*d)'''
 
+'''def check1(*a):
+    d=1 #creating a variable
+    print(a)
+    print(type(a))
+    for i in a:
+        if type(i) in (int,float):
+            d = d+i
+            print(d)
+check1()
+check1(1,2,3,4,5,6,7,8)
+check1(4,3,6,2.3,45.7)
+check1(4,3,6,2.3,45.7,"python")'''
+        
+# ** (kwargs)
+# keyword variable length arguments
+'''def check2(**a):
+    print(a)
+    print(type(a))
+check2()
+details = {"names":["sweety","suji","viji","suni"],
+           "marks":[80,81,82,83]
+           "status":["p","a","p","a"]}
+check2(**details)'''
 
+'''def check3(**a):
+    print(a)
+    print(type(a))
+    for i in a: 
+        print(i)
+    for i in a.keys():
+        print(i)
+    for i in a:
+        print(a[i])
+    for i in a.values():
+        print(i)
+    for i in a:
+        print(i,a[i])
+    for i in a.items():
+        print(i)
+check3()
+details = {"names":["sweety","suji","viji","suni"],
+           "marks":[80,81,82,83],
+           "status":["p","a","p","a"]}
+check3(**details)'''
 
+# both * & ** usage in 1 program
+'''def final(*a,**b):
+    d=2
+    print(a)
+    print(b)
+    print(type(a))
+    print(type(b))
+    for i in a:
+        d=d+1
+        print(d)
+    for i,j in b.items():
+        print("key is",i)
+        print("value is",j)
+final()
+data=(2,3,4,5,6,2.3,4.5)
+# final(*data) """this is for single print of data but for details we need print the another line"""
+details={"year":[2024,2026,2028],
+         "month":["june","july","sept"]}
+# final(**details) """this is for single print of details but for data we need print the another line"""
+final(*data,**details)'''
 
-
-
-
-
-
-
-
+# Railway Ticket
+'''while True:
+    def Railway():
+        Gender = input("Enter the Gender: ")
+        t=int(input("Enter the price of ticket: "))
+        if Gender == "M":
+            print("Enter the Age of Male: ")
+            age=int(input())
+            if age >= 60:
+                print(t-30/100*t)
+            else:
+                print("no discount {t}")
+        else:
+            print("ticket")
+        if Gender == "F":
+            print("Enter the Age of Female: ")
+            age = int(input())
+            if age >= 60:
+                print(t-50/100*t)
+            if age < 60:
+                print(t-30/100*t)
+        else:
+            ("ticket")
+    Railway()'''

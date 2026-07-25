bf9d8bae2de0229760c9da8ce7472d7277e4690e
garikapati-vijitha <garikapativijithachowdary2004@gmail.com>
2026-07-25 14:20:23 +0530

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,64 @@
+# d/f b/w module & function & packages
+# module -> a module in python is a single python file it consist python core.
+# it tipically consist of functions, classes & variables that can be used in other puthon scripts or programs (or) contains pyhon code.
+# examples of modules ->math.py, random.py (or) my module.py.
+
+# Packages -> a package in python it is a diretory cantaining one (or) more python modules and an "__init__.py" file.
+# The "__init__.py" file can be empty (or) contain intilization code for the package.
+# enxamples of packages -> numpy, pandas, djongo & requestes.
+
+# library -> it consists of both modules and packages.
+# examples of library -> numpy, pandas & matplotlib.
+
+# Note: every python file is module & import is a keyword & every python file is saved internally with variable name as "__main__".
+# pyhton location -> C:\Users\User\AppData\Local\Programs\Python\Python314>
+
+'''def greetings(name):
+    print("welcome",name)'''
+
+'''a=4
+b=8
+print(a+b)'''
+
+
+'''a=int(input())
+b=int(input())
+print(a+b)'''
+
+'''details={"idnos":[10,20,30],
+        "names":["vijitha","srujana","yashu"],
+        "marks":[98,99,99]}'''
+
+'''if __name__=="__main__":
+    a=[10,20,30,40,50]
+    a.append("code")
+    a.append("code")
+    print(a)'''
+
+'''def dummy():
+    if __name__ == "__main__":
+        print("this code is run as script")
+    else:
+        print("this program is run as module")
+dummy()'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ modules_output.py
@@ -0,0 +1,14 @@
+'''import modules
+modules.greetings("vijitha")'''
+
+#import modules
+#import modules
+
+'''import modules
+b=modules.details["idnos"]
+c=modules.details["names"]
+print(b,c)'''
+
+#import modules
+
+
